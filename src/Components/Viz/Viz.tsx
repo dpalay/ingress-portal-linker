b4503a64e1a2d9336dec5aba9d143afc3f075d27
Dave Palay <dpalay@epic.com>
2019-10-09 12:59:54 -0500

    get data from file

--- src/Components/Viz/Viz.tsx
+++ src/Components/Viz/Viz.tsx
@@ -1,10 +1,23 @@
 import React, {useEffect, useRef} from 'react';
 import './Viz.css';
 import * as d3 from 'd3'
-import { appendFile } from 'fs';
+
 
 interface IProps {
     data?: {count: number}[]
+}
+
+interface DataFromJSON {
+    "guid": string
+    "title":string
+    "coordinates":{
+        "lat":string | number
+        "lng":string | number}
+    "link":{
+        "intel":string
+        "gmap":string
+    }
+    "image": string
 }
 
 const Viz: React.FC<IProps> = (props: IProps)  => {
@@ -16,51 +29,70 @@ const Viz: React.FC<IProps> = (props: IProps)  => {
 
 
     useEffect(() => {
-        if (props.data && d3Container.current) {
+        if (props.data && props.data[0] && d3Container.current) {
             const svg = d3.select(d3Container.current);
+            const length = props.data[0].count
             // set width/height of SVG
             svg.attr('width', 500).attr('height',500)
             
-            // get data from props
-            const data = dataset.slice(0,props.data[0].count)
-    
+            
             //setup ranges
-            let x = d3.scaleLinear().domain([0,1]).range([10,490])
             
-            const update = svg.selectAll("g.circleContainer").data(data)
-            
-            // When removing an entry
-            update.exit().remove();
+            d3.json('data.json').then( (dataFromJSON: DataFromJSON[]) => {
+                
+                
+                let x =d3.scaleLinear().domain(
+                    //@ts-ignore
+                    d3.extent(dataFromJSON, function(d) {
+                      return +d.coordinates.lng;
+                    })).nice().range([0,480])
 
+                let y = d3.scaleLinear().domain(
+                    //@ts-ignore
+                    d3.extent(dataFromJSON, function(d) {
+                      return +d.coordinates.lat;
+                    })
+                  ).nice().range([0,480]);
+                
+                // get data from props
+                const data = dataFromJSON.slice(0,length)
 
-            // update existing elements
-            update.attr("new", "false")
-            .transition()
+                const update = svg.selectAll("g.circleContainer").data(data)
+                
+                // When removing an entry
+                update.exit().remove();
+                
+                
+                // update existing elements
+                update.attr("new", "false")
+                .transition()
                 .duration(500)
                 .ease(d3.easeQuadInOut)
-                .attr("transform", (datum, i , arr) => `translate(${x((i+1)/(arr.length+1))},${Math.random()*500})`)
+                .attr("transform", (datum, i , arr) => `translate(${x(+datum.coordinates.lng)},${y(+datum.coordinates.lat)})`)
                 .style("fill", (d,i,arr) => (d3.interpolateRainbow((i+1)/(arr.length+1))))
                 .style("stroke", "none")
-            
-
-            // Adding new elements
-            update.enter()
-            .append("g").attr("class", "circleContainer")
-            .attr("new", "true")
-            .attr("transform", (datum, i , arr) => `translate(${x((i+1)/(arr.length+1))},200)`)
-            .style("fill", (d,i,arr) => (d3.interpolateRainbow((i+1)/(arr.length+1))))
-            .style("stroke", "black")
-            .data(data)
-            .append("circle")
-            .attr("r", 10)
-            //@ts-ignore
-            // the merge returns the g, not the circle
-            .merge(update)
-            // This should happen to both old and new
-            //.attr("r", Math.random()*15 + 5)
-    
-    }
-}, [props.data, d3Container.current])
+                
+                
+                // Adding new elements
+                update.enter()
+                .append("g").attr("class", "circleContainer")
+                .attr("new", "true")
+                .attr("transform", (datum, i , arr) => `translate(${200},${200})`)
+                .style("fill", (d,i,arr) => (d3.interpolateRainbow((i+1)/(arr.length+1))))
+                .style("stroke", "black")
+                .data(data)
+                .append("circle")
+                .attr("r", 3)
+                //@ts-ignore
+                // the merge returns the g, not the circle
+                .merge(update)
+                // This should happen to both old and new
+                //.attr("r", Math.random()*15 + 5)
+                
+                
+            })
+            }
+        }, [props.data, d3Container.current])
     return (<div className="viz">
         <svg className="d3-component" ref={d3Container}/>
     </div>)
