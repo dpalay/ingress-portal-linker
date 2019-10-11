86fcbaf2775a732375325e8aecae6c06cdd1ac31
Dave Palay <dpalay@epic.com>
2019-10-11 09:17:13 -0500

    Dead-end.  Can't do line segments in react-vis

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -5,13 +5,22 @@ import './App.css';
 import './style.css'
 import myClickFunction from '../../Utils/events'
 import TestControl from '../TestControl/TestControl';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis'
+import {XAxis, YAxis, HorizontalGridLines, MarkSeries, FlexibleXYPlot, CustomSVGSeries} from 'react-vis'
+import data from '../../Data/PortalList'
+import allLinks from '../../Data/allLinks'
+import {scaleLinear} from 'd3-scale'
 
-
+let {Highlight} = require('react-vis')
+interface IDrawLocation{
+  left: number
+  right: number
+  top: number
+  bottom: number
+}
 
 const {Header, Footer, Content, Sider} = Layout
 
-const initialState = {count: 3}
+const initialState = {count: 0}
 
   const handleClickReducer = (state: {count: number}, action: {type: string})  => {
     switch (action.type){
@@ -26,8 +35,8 @@ const initialState = {count: 3}
 
 const App: React.FC = () => {
   
+    let [lastDrawLocation, setLastDrawLocation] = useState({} as IDrawLocation)
     let [value, dispatch] = useReducer(handleClickReducer, initialState);
-
 
   return (
     <div>
@@ -41,25 +50,51 @@ const App: React.FC = () => {
           <Content>
             <TestControl text="Add a ball" value={value.count} handleclick={() => dispatch({type: 'increment'})}/>
             <TestControl text="Remove a ball" value={value.count} handleclick={() => dispatch({type: 'decrement'})}/>
-            <XYPlot
+            <FlexibleXYPlot
+            animation
+            xDomain={
+              (lastDrawLocation && lastDrawLocation.left && lastDrawLocation.right ) && [
+                lastDrawLocation.left,
+                lastDrawLocation.right
+              ]
+            }
+            yDomain={
+              (lastDrawLocation && lastDrawLocation.bottom && lastDrawLocation.top) && [
+                lastDrawLocation.bottom,
+                lastDrawLocation.top
+              ]
+            }
   width={600}
   height={600}>
   <HorizontalGridLines />
-  <LineSeries
-    data={[
-      {x: 1, y: 10},
-      {x: 2, y: 5},
-      {x: 3, y: 15}
-    ]}/>
-  <LineSeries
-  data={[{x:1,y:5},{x:4,y:20}]}
-  color={"#ee6600"}
+  <MarkSeries data={data.map(datum => {return {x: datum.coordinates.lng, y: datum.coordinates.lat}})}></MarkSeries>
   
-  />
-
   <XAxis />
   <YAxis />
-</XYPlot>
+
+  <Highlight
+              onBrushEnd={(area: IDrawLocation) => setLastDrawLocation(area)}
+              onDrag={(area: IDrawLocation) => {
+                setLastDrawLocation({
+                    bottom: lastDrawLocation.bottom + (area.top - area.bottom),
+                    left: lastDrawLocation.left - (area.right - area.left),
+                    right: lastDrawLocation.right - (area.right - area.left),
+                    top: lastDrawLocation.top + (area.top - area.bottom)
+                  })
+                }}
+            />
+
+<CustomSVGSeries data={allLinks.map(link => {
+  //@ts-ignore
+let xScale = scaleLinear().domain(xDomain).range(xRange)
+return(
+  {x:link.source.x, y:link.source.y, customComponent: 'circle'}
+)
+
+})}>
+
+</CustomSVGSeries>
+</FlexibleXYPlot>
           </Content>
         </Layout>
         <Footer>
