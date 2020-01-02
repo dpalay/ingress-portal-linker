1cdd3b4fca3f09ac2e0797ecf125b41896ab07e8
Dave Palay <dpalay@epic.com>
2020-01-02 16:24:27 -0600

    Working on the object side

--- src/Utils/Objects/Portal.ts
+++ src/Utils/Objects/Portal.ts
@@ -1,21 +1,31 @@
-import  Point from "./Point";
+import Point from "./Point";
 import Link from "./Link";
 
 class Portal {
-    location: Point;
-    title: string;
+  location: Point;
+  title: string;
 
-    constructor(location: Point, title: string) {
-        this.location = new Point();
-        this.location.set(location);
-        this.title = title;
-    }
+  constructor(location: Point, title: string) {
+    this.location = new Point();
+    this.location.set(location);
+    this.title = title;
+  }
 
-    linkTo(otherPortal: Portal): Link {
-        return new Link(this, otherPortal)
-    }
+  linkTo(otherPortal: Portal): Link {
+    return new Link(this, otherPortal);
+  }
 
+  public get x(): number {
+    return this.location.x;
+  }
 
+  public get y(): number {
+    return this.location.y;
+  }
+
+  public get pointXY(): [number, number] {
+    return [this.location.x, this.location.y];
+  }
 }
 
-export default Portal
+export default Portal;
