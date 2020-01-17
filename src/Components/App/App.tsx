01126cec17c53b2d6cff2ead9ce2a1a877a5882d
dpalay <lordthanda@gmail.com>
2020-01-16 23:45:35 -0600

    routes

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useMemo } from "react";
-import { Row, Col, Layout, Button } from "antd";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { Row, Col, Layout, Button, Affix } from "antd";
 import "./App.css";
 import Viz from "../Viz/Viz";
 import AnchorSelect from "../AnchorSelect/AnchorSelect";
@@ -9,10 +10,15 @@ import DebugInfo from "../DebugInfo/DebugInfo";
 import PortalEntry from "../PortalEntry/PortalEntry";
 import Portal from "../../Utils/Objects/Portal";
 import CardResults from "../CardResults/CardResults";
-import Link from "../../Utils/Objects/Link";
+import PortalLink from "../../Utils/Objects/Link";
 
 type IDirection = "East" | "West" | "North" | "South";
-interface ICompiledPortal { portal: Portal, linksFrom: Link[], linksTo: Link[], numKeys: number }
+interface ICompiledPortal {
+  portal: Portal;
+  linksFrom: PortalLink[];
+  linksTo: PortalLink[];
+  numKeys: number;
+}
 interface IRawData {
   guid: string;
   title: string;
@@ -34,15 +40,12 @@ const { Header, Footer, Content } = Layout;
 //const initialCount = 5
 
 const App: React.FC = () => {
-  const [whichAnchor, setWhichAnchor] = useState(
-    anchorDefault as IDirection
-  );
+  const [whichAnchor, setWhichAnchor] = useState(anchorDefault as IDirection);
   const [whichPrimary, setWhichPrimary] = useState(
     primaryDefault as IDirection
   );
   const [selected, setSelected] = useState(0);
   const [rawData, setRawData] = useState<IRawData[]>(rawPortals);
-
   /*
     const handleClickReducer = (count: number, action: { type: string }) => {
       switch (action.type) {
@@ -79,12 +82,11 @@ const App: React.FC = () => {
     [rawData, value]
   );
 
-
   //  const [allLinks, setAllLinks] = useState<Link[]>([])
 
-
-  const [compiledPortalInfo, allLinks, anchor] = useMemo<[ICompiledPortal[], Link[], Portal]>(() => {
-
+  const [compiledPortalInfo, allLinks, anchor] = useMemo<
+    [ICompiledPortal[], PortalLink[], Portal]
+  >(() => {
     let anchor: Portal = data.reduce((prev, cur) => {
       switch (whichAnchor) {
         case "West":
@@ -120,36 +122,37 @@ const App: React.FC = () => {
         break;
     }
 
-    sortedAvailablePortals.sort(
-      (a, b) => {
-        switch (whichPrimary) {
-          case "East":
-          case "North":
-            return a.slopeFromAnchor - b.slopeFromAnchor;
+    sortedAvailablePortals.sort((a, b) => {
+      switch (whichPrimary) {
+        case "East":
+        case "North":
+          return a.slopeFromAnchor - b.slopeFromAnchor;
 
-          case "West":
-          case "South":
-            return b.slopeFromAnchor - a.slopeFromAnchor;
+        case "West":
+        case "South":
+          return b.slopeFromAnchor - a.slopeFromAnchor;
 
-          default:
-            return 0;
-        }
+        default:
+          return 0;
       }
-    );
+    });
 
-    let compiledPortalInfo: ICompiledPortal[] =
-      [{ portal: anchor, linksFrom: [], linksTo: [], numKeys: 0 }, ...sortedAvailablePortals.map((p: Portal) => { return { portal: p, linksFrom: [], linksTo: [], numKeys: 0 }; })];
+    let compiledPortalInfo: ICompiledPortal[] = [
+      { portal: anchor, linksFrom: [], linksTo: [], numKeys: 0 },
+      ...sortedAvailablePortals.map((p: Portal) => {
+        return { portal: p, linksFrom: [], linksTo: [], numKeys: 0 };
+      })
+    ];
 
-    let tmpAllLinks: Link[] = [];
+    let tmpAllLinks: PortalLink[] = [];
 
     sortedAvailablePortals.forEach((p: Portal, i: number) => {
-      let tmpLink = new Link(p, anchor);
+      let tmpLink = new PortalLink(p, anchor);
       tmpAllLinks.push(tmpLink);
       compiledPortalInfo[0].linksTo.push(tmpLink);
       compiledPortalInfo[0].numKeys++;
       compiledPortalInfo[i + 1].linksFrom.push(tmpLink);
     });
-
 
     for (
       let sourcePortalIndex = 1;
@@ -164,7 +167,7 @@ const App: React.FC = () => {
         destPortalIndex++
       ) {
         const destPortal = sortedAvailablePortals[destPortalIndex];
-        const tmpLink = new Link(sourcePortal, destPortal);
+        const tmpLink = new PortalLink(sourcePortal, destPortal);
         if (!tmpAllLinks.some(link => link.intersect(tmpLink))) {
           tmpAllLinks.push(tmpLink);
           compiledPortalInfo[sourcePortalIndex + 1].linksFrom.push(tmpLink);
@@ -175,88 +178,108 @@ const App: React.FC = () => {
     }
 
     return [compiledPortalInfo, tmpAllLinks, anchor];
-  }
-
-    , [data, whichAnchor, whichPrimary]);
-
+  }, [data, whichAnchor, whichPrimary]);
 
   return (
-    <div>
-      <Layout>
-        <Header className="ingress-frame dark-back">
-          <Row type="flex">
-            <Button>{"Show sidebar"}</Button>
-            <h2>Dave&apos;s Portal Linker</h2>
-          </Row>
-        </Header>
+    <Router>
+      <div>
+        <Layout>
+          <Header className="ingress-frame dark-back">
+            <Row type="flex" justify="center" gutter={2}>
+              <Col>
+              <Button onClick={(e) => console.log(e)}>{"Show sidebar"}</Button>
+              </Col>
+              <Col>
+              <h2>Dave&apos;s Portal Linker</h2>
+              </Col>
+              <Col>
+              </Col>
+            </Row>
+          </Header>
 
-        <Content className="ingress-frame dark-back">
-          <Row type="flex">
-            <Col span={7}>
-              {" "}
-              {/**TODO: Make this a Drawer at some point in the future */}
-              <div className={"ingress-frame padded"}>
+          <Content className="ingress-frame dark-back">
+            <Row type="flex">
+              <Col span={7}>
+                {" "}
+                {/**TODO: Make this a Drawer at some point in the future */}
+                <div className={"ingress-frame padded"}>
+                  <Row justify="center">
+                    <Col lg={{span: 4, push:20}}>
+                      <div className="ingress-text">
+                      <Link to="/">Home</Link>
+                      <Link to="/about">About</Link>
+                      <Link to="/view">Show Map</Link>
+                      </div>
+                    </Col>
+                    <Col lg={{ span: 10, pull: 4 }} md={{ span: 20 }}>
+                      <AnchorSelect
+                        title={"Anchor Portal"}
+                        which={whichAnchor}
+                        setWhich={setWhichAnchor}
+                      />
+                    </Col>
+                    <Col lg={{ span: 10 , pull: 4}} md={{ span: 20 }}>
+                      <PrimarySelect
+                        title={"Start From Direction"}
+                        which={whichPrimary}
+                        setWhich={setWhichPrimary}
+                        whichAnchor={whichAnchor}
+                      />
+                    </Col>
+                  </Row>
+                </div>
                 <Row>
-                  <Col lg={{ span: 10 }} md={{ span: 20 }}>
-                    <AnchorSelect
-                      title={"Anchor Portal"}
-                      which={whichAnchor}
-                      setWhich={setWhichAnchor}
-                    />
-                  </Col>
-                  <Col lg={{ span: 10 }} md={{ span: 20 }}>
-                    <PrimarySelect
-                      title={"Start From Direction"}
-                      which={whichPrimary}
-                      setWhich={setWhichPrimary}
-                      whichAnchor={whichAnchor}
+                  <Col>
+                    <PortalEntry
+                      text={JSON.stringify(rawPortals)}
+                      rawData={rawData}
+                      setRawData={setRawData}
                     />
                   </Col>
                 </Row>
-              </div>
-              <Row>
-                <Col>
-                  <PortalEntry
-                    text={JSON.stringify(rawPortals)}
-                    rawData={rawData}
-                    setRawData={setRawData}
+                <Row>
+                  <Col>
+                    <CardResults compiledPortals={compiledPortalInfo} />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <DebugInfo
+                      selected={selected}
+                      data={data}
+                      valueOfSlider={value}
+                      whichAnchor={whichAnchor}
+                    ></DebugInfo>
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={17}>
+              <Switch>
+                <Route path="/view">
+
+                <Affix offsetTop={10}>
+                <Viz
+                  data={data}
+                  whichAnchor={whichAnchor}
+                  whichPrimary={whichPrimary}
+                  setSelected={setSelected}
+                  allLinks={allLinks}
+                  anchor={anchor}
                   />
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <CardResults compiledPortals={compiledPortalInfo} />
-                </Col>
+                  </Affix>
+                  </Route>
+                  <Route path="/">
+                    <div className="ingress-text">Home screen</div>
+                  </Route>
+                </Switch>
+              </Col>
+            </Row>
+          </Content>
 
-              </Row>
-              <Row>
-                <Col>
-                  <DebugInfo
-                    selected={selected}
-                    data={data}
-                    valueOfSlider={value}
-                    whichAnchor={whichAnchor}
-                  ></DebugInfo>
-                </Col>
-              </Row>
-            </Col>
-            <Col span={17}>
-              <Viz
-                data={data}
-                whichAnchor={whichAnchor}
-                whichPrimary={whichPrimary}
-                setSelected={setSelected}
-                allLinks={allLinks}
-                anchor={anchor}
-
-              />
-            </Col>
-          </Row>
-        </Content>
-
-        <Footer className="ingress-frame dark-back"></Footer>
-      </Layout>
-    </div>
+          <Footer className="ingress-frame dark-back"></Footer>
+        </Layout>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/Utils/routes.ts
@@ -0,0 +1,6 @@
+import {Route} from "react-router-dom"
+
+const routes: Array<Route> = [
+
+];
+export default routes;
